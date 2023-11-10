06ea0dd48088ab9b7ac78678ace0a56b87a3371f
peteross <peteross.sax@gmail.com>
2023-11-10 09:09:25 -0600

    add a search bar, it doesn't super work

--- app/src/main/java/com/alltrails/lunch/app/ui/MainScreen.kt
+++ app/src/main/java/com/alltrails/lunch/app/ui/MainScreen.kt
@@ -4,20 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.alltrails.lunch.app.R
+import com.alltrails.lunch.app.ui.theme.Padding1_5x
+import com.alltrails.lunch.app.ui.theme.Padding1x
+import com.alltrails.lunch.app.ui.theme.SearchbarHeight
 import com.alltrails.lunch.app.viewModel.MainViewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -38,7 +45,7 @@ fun MainScreen(viewModel : MainViewModel = koinViewModel()) {
           ) {
             Image(
               modifier = Modifier.wrapContentSize(),
-              painter = painterResource(id = R.drawable.logo_lockup),
+              painter = painterResource(id = R.drawable.logo),
               contentDescription = null,
             )
           }
@@ -61,10 +68,30 @@ fun MainScreen(viewModel : MainViewModel = koinViewModel()) {
         val cameraPositionState = rememberCameraPositionState {
           position = CameraPosition.fromLatLngZoom(singapore, 10f)
         }
-        GoogleMap(
-          modifier = Modifier.fillMaxSize(),
-          cameraPositionState = cameraPositionState
-        )
+        SearchBar(
+          query = "ffdr",
+          onQueryChange = { /* No autocomplete for now */ },
+          onSearch = {},
+          modifier = Modifier
+            .padding(
+              start = Padding1_5x,
+              end = Padding1_5x,
+              bottom = Padding1x,
+            )
+            .clip(shape = RoundedCornerShape(Padding1x))
+            .height(SearchbarHeight)
+          ,
+          placeholder = {
+            Text(text = "Search restaurants")
+          },
+          active = true,
+          onActiveChange = {},
+          leadingIcon = { Image(painter = painterResource(id = R.drawable.search), null)}
+        ) {}
+          GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+          )
       }
     )
   }
--- app/src/main/java/com/alltrails/lunch/app/ui/theme/Dimension.kt
+++ app/src/main/java/com/alltrails/lunch/app/ui/theme/Dimension.kt
@@ -3,8 +3,11 @@ package com.alltrails.lunch.app.ui.theme
 import androidx.compose.ui.unit.dp
 
 val Padding1x = 16.dp
+val Padding1_5x = 24.dp
 val PaddingInner = 12.dp
 val SpacingSmall = 4.dp
+
+val SearchbarHeight = 32.dp
 
 val RestaurantItemImageHeight = 72.dp
 val RestaurantItemImageWidth = 64.dp
